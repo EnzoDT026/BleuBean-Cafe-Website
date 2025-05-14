00318285d576ca17d796037ec88611fd48219056
EnzoDT026 <172428982+EnzoDT026@users.noreply.github.com>
2025-05-15 01:50:26 +0800

    update responsiveness

---
+++ BleuBeanWebsite/src/scripts/Products.js
@@ -0,0 +1,12 @@
+ function toggleVisibility() 
+ {
+     var x = document.getElementById("myDIV");
+     if (x.style.display === "none") 
+        {
+            x.style.display = "block";
+        } 
+        else 
+        {
+            x.style.display = "none";
+        }
+    }                   
